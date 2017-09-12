bafee32ab83caf306abe20b1408680b3036dd453
prayansh1996 <prayansh1996@gmail.com>
2017-09-12 21:12:02 +0530

    python3 controller.py

---
+++ crawler-webApp/js/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('#modal1').modal();
+      
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
